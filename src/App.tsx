53f4af1b0ef2f39d4582fab9120308c7bad37fa5
Ayaka Miyake <ayakamiyake7@gmail.com>
2022-05-08 20:32:10 +0100

    Add UI.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "antd/dist/antd.css";
+import { Input, Typography, Layout, Button, Select } from "antd";
+
+const { Text, Title } = Typography;
+const { Header } = Layout;
+const { Option } = Select;
+
+type Todo = {
+  Option: string;
+};
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todoText, setTodoText] = useState("");
+
+  const handleInputChange = (e: any) => {
+    setTodoText(e.target.value);
+    console.log(e.target.value);
+  };
+  const handleClickCreate = () => {
+    if (todoText === "") return;
+    const newTodo = {
+      text: todoText,
+      status: "Not Started",
+    };
+    // const newTodos = [...todos, todoText];
+    // setTodos(newTodos);
+    setTodos([
+      // copy the current values in state
+      ...todos,
+    ]);
+    console.log("todos-", todos);
+    console.log("todoText=", todoText);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header
+        style={{
+          backgroundColor: "#ff7a45",
+          height: "auto",
+          padding: "10px 50px",
+          margin: "0 0 40px",
+        }}
+      >
+        <Title style={{ color: "#fff", margin: 0 }}>Todo List</Title>
+      </Header>
+      <Input
+        placeholder="Enter a todo"
+        type="text"
+        value={todoText}
+        onChange={handleInputChange}
+        style={{ width: "20%", marginRight: "20px" }}
+      />
+      <Button onClick={handleClickCreate}>Create</Button>
+      <ul style={{ width: "50%", margin: "20px auto 0" }}>
+        <li style={{ textAlign: "left" }}>
+          <Text style={{ marginRight: "10px" }}>Test</Text>
+          <Select style={{ width: "30%", marginRight: "20px" }}>
+            <Option value="Not Started">Not Started</Option>
+            <Option value="Not Started">In Progress</Option>
+            <Option value="Not Started">Completed</Option>
+          </Select>
+          <Button style={{ marginRight: "10px" }}>Update</Button>
+          <Button>Delete</Button>
+        </li>
+        {todos.map((todo, index) => (
+          <li key={index} style={{ textAlign: "center" }}>
+            {todo}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
